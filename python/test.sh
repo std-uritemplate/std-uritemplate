2e69402c58812f3702ae8f6abc6dba3ede6e138d
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-07-13 12:55:52 +0100

    scaffolding a chatGPT Python impl

---
+++ python/test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# This is intended to be a customizable entrypoint for each language, it has to be generic enough
+(
+  cd ${SCRIPT_DIR} && \
+  poetry install -q && \
+  poetry run python test.py $@
+)
