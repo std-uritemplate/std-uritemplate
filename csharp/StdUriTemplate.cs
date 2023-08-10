e1450f6a731bd71cd3375e465ac1146017f39509
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-08-10 18:54:45 +0100

    Initial C# scaffolded implementation

---
+++ csharp/StdUriTemplate.cs
@@ -0,0 +1,572 @@
+﻿namespace stduritemplate;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class StdUriTemplate
+{
+    // Public API
+    public static string Expand(string template, Dictionary<string, object> substitutions)
+    {
+        return ExpandImpl(template, substitutions);
+    }
+
+    // Private implementation
+    private enum Modifier
+    {
+        NO_MOD,
+        PLUS,
+        DASH,
+        DOT,
+        SLASH,
+        SEMICOLON,
+        QUESTION_MARK,
+        AT
+    }
+
+    private static void ValidateLiteral(char c, int col)
+    {
+        switch (c)
+        {
+            case '+':
+            case '#':
+            case '/':
+            case ';':
+            case '?':
+            case '&':
+            case ' ':
+            case '!':
+            case '=':
+            case '$':
+            case '|':
+            case '*':
+            case ':':
+            case '~':
+            case '-':
+                throw new ArgumentException($"Illegal character identified in the token at col:{col}");
+            default:
+                break;
+        }
+    }
+
+    private static int GetMaxChar(StringBuilder buffer, int col)
+    {
+        if (buffer == null)
+        {
+            return -1;
+        }
+        else
+        {
+            string value = buffer.ToString();
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return -1;
+            }
+            else
+            {
+                try
+                {
+                    return int.Parse(value);
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException($"Cannot parse max chars at col:{col}");
+                }
+            }
+        }
+    }
+
+    private static Modifier GetModifier(char c, StringBuilder token, int col)
+    {
+        switch (c)
+        {
+            case '+': return Modifier.PLUS;
+            case '#': return Modifier.DASH;
+            case '.': return Modifier.DOT;
+            case '/': return Modifier.SLASH;
+            case ';': return Modifier.SEMICOLON;
+            case '?': return Modifier.QUESTION_MARK;
+            case '&': return Modifier.AT;
+            default:
+                ValidateLiteral(c, col);
+                token.Append(c);
+                return Modifier.NO_MOD;
+        }
+    }
+
+    private static string ExpandImpl(string str, Dictionary<string, object> substitutions)
+    {
+        StringBuilder result = new StringBuilder(str.Length * 2);
+
+        StringBuilder token = null;
+
+        Modifier modifier = Modifier.NO_MOD;
+        bool composite = false;
+        StringBuilder maxCharBuffer = null;
+        bool firstToken = true;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            char character = str[i];
+            switch (character)
+            {
+                case '{':
+                    token = new StringBuilder();
+                    firstToken = true;
+                    break;
+                case '}':
+                    if (token != null)
+                    {
+                        bool expanded = ExpandToken(modifier, token.ToString(), composite, GetMaxChar(maxCharBuffer, i), firstToken, substitutions, result, i);
+                        if (expanded && firstToken)
+                        {
+                            firstToken = false;
+                        }
+                        token = null;
+                        modifier = Modifier.NO_MOD;
+                        composite = false;
+                        maxCharBuffer = null;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Failed to expand token, invalid at col:{i}");
+                    }
+                    break;
+                case ',':
+                    if (token != null)
+                    {
+                        bool expanded = ExpandToken(modifier, token.ToString(), composite, GetMaxChar(maxCharBuffer, i), firstToken, substitutions, result, i);
+                        if (expanded && firstToken)
+                        {
+                            firstToken = false;
+                        }
+                        token = new StringBuilder(token.Length * 2);
+                        composite = false;
+                        maxCharBuffer = null;
+                        break;
+                    }
+                    // Intentional fall-through for commas outside the {}
+                    goto default;
+                default:
+                    if (token != null)
+                    {
+                        if (modifier == Modifier.NO_MOD)
+                        {
+                            modifier = GetModifier(character, token, i);
+                        }
+                        else if (maxCharBuffer != null)
+                        {
+                            if (char.IsDigit(character))
+                            {
+                                maxCharBuffer.Append(character);
+                            }
+                            else
+                            {
+                                throw new ArgumentException($"Illegal character identified in the token at col:{i}");
+                            }
+                        }
+                        else
+                        {
+                            if (character == ':')
+                            {
+                                maxCharBuffer = new StringBuilder(3);
+                            }
+                            else if (character == '*')
+                            {
+                                composite = true;
+                            }
+                            else
+                            {
+                                ValidateLiteral(character, i);
+                                token.Append(character);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        result.Append(character);
+                    }
+                    break;
+            }
+        }
+
+        if (token == null)
+        {
+            return result.ToString();
+        }
+        else
+        {
+            throw new ArgumentException("Unterminated token");
+        }
+    }
+
+    private static void AddPrefix(Modifier mod, StringBuilder result)
+    {
+        switch (mod)
+        {
+            case Modifier.DASH:
+                result.Append('#');
+                break;
+            case Modifier.DOT:
+                result.Append('.');
+                break;
+            case Modifier.SLASH:
+                result.Append('/');
+                break;
+            case Modifier.SEMICOLON:
+                result.Append(';');
+                break;
+            case Modifier.QUESTION_MARK:
+                result.Append('?');
+                break;
+            case Modifier.AT:
+                result.Append('&');
+                break;
+            default:
+                return;
+        }
+    }
+
+    private static void AddSeparator(Modifier mod, StringBuilder result)
+    {
+        switch (mod)
+        {
+            case Modifier.DOT:
+                result.Append('.');
+                break;
+            case Modifier.SLASH:
+                result.Append('/');
+                break;
+            case Modifier.SEMICOLON:
+                result.Append(';');
+                break;
+            case Modifier.QUESTION_MARK:
+            case Modifier.AT:
+                result.Append('&');
+                break;
+            default:
+                result.Append(',');
+                break;
+        }
+    }
+
+    private static void AddValue(Modifier mod, string token, string value, StringBuilder result, int maxChar)
+    {
+        switch (mod)
+        {
+            case Modifier.PLUS:
+            case Modifier.DASH:
+                AddExpandedValue(value, result, maxChar, false);
+                break;
+            case Modifier.QUESTION_MARK:
+            case Modifier.AT:
+                result.Append(token + '=');
+                AddExpandedValue(value, result, maxChar, true);
+                break;
+            case Modifier.SEMICOLON:
+                result.Append(token);
+                if (!string.IsNullOrEmpty(value))
+                {
+                    result.Append('=');
+                }
+                AddExpandedValue(value, result, maxChar, true);
+                break;
+            case Modifier.DOT:
+            case Modifier.SLASH:
+            case Modifier.NO_MOD:
+                AddExpandedValue(value, result, maxChar, true);
+                break;
+        }
+    }
+
+    private static void AddValueElement(Modifier mod, string token, string value, StringBuilder result, int maxChar)
+    {
+        switch (mod)
+        {
+            case Modifier.PLUS:
+            case Modifier.DASH:
+                AddExpandedValue(value, result, maxChar, false);
+                break;
+            case Modifier.QUESTION_MARK:
+            case Modifier.AT:
+            case Modifier.SEMICOLON:
+            case Modifier.DOT:
+            case Modifier.SLASH:
+            case Modifier.NO_MOD:
+                AddExpandedValue(value, result, maxChar, true);
+                break;
+        }
+    }
+
+    private static void AddExpandedValue(string value, StringBuilder result, int maxChar, bool replaceReserved)
+    {
+        int max = (maxChar != -1) ? Math.Min(maxChar, value.Length) : value.Length;
+        result.EnsureCapacity(max * 2); // hint to SB
+        StringBuilder reservedBuffer = null;
+
+        for (int i = 0; i < max; i++)
+        {
+            char character = value[i];
+
+            if (character == '%' && !replaceReserved)
+            {
+                reservedBuffer = new StringBuilder(3);
+            }
+
+            if (reservedBuffer != null)
+            {
+                reservedBuffer.Append(character);
+
+                if (reservedBuffer.Length == 3)
+                {
+                    bool isEncoded = false;
+                    try
+                    {
+                        Uri.UnescapeDataString(reservedBuffer.ToString());
+                        isEncoded = true;
+                    }
+                    catch (Exception)
+                    {
+                        // ignore
+                    }
+
+                    if (isEncoded)
+                    {
+                        result.Append(reservedBuffer);
+                    }
+                    else
+                    {
+                        result.Append("%25");
+                        // only if !replaceReserved
+                        result.Append(reservedBuffer.ToString(1, 2));
+                    }
+                    reservedBuffer = null;
+                }
+            }
+            else
+            {
+                if (character == ' ')
+                {
+                    result.Append("%20");
+                }
+                else if (character == '%')
+                {
+                    result.Append("%25");
+                }
+                else
+                {
+                    if (replaceReserved)
+                    {
+                        result.Append(Uri.EscapeDataString(character.ToString()));
+                    }
+                    else
+                    {
+                        result.Append(character);
+                    }
+                }
+            }
+        }
+
+        if (reservedBuffer != null)
+        {
+            result.Append("%25");
+            if (replaceReserved)
+            {
+                result.Append(Uri.EscapeDataString(reservedBuffer.ToString(1, reservedBuffer.Length - 1)));
+            }
+            else
+            {
+                result.Append(reservedBuffer.ToString(1, reservedBuffer.Length - 1));
+            }
+        }
+    }
+
+    private static bool IsList(object value)
+    {
+        return value is List<string> ||
+            value is List<object>; // checking concrete instances first as it's faster
+    }
+
+    private static bool IsDictionary(object value)
+    {
+        return value is Dictionary<string, string> ||
+            value is Dictionary<string, object>; // checking concrete instances first as it's faster
+    }
+
+    private enum SubstitutionType
+    {
+        STRING,
+        LIST,
+        DICTIONARY
+    }
+
+    private static SubstitutionType GetSubstitutionType(object value, int col)
+    {
+        if (value is string || value == null)
+        {
+            return SubstitutionType.STRING;
+        }
+        else if (IsList(value))
+        {
+            return SubstitutionType.LIST;
+        }
+        else if (IsDictionary(value))
+        {
+            return SubstitutionType.DICTIONARY;
+        }
+        else
+        {
+            throw new ArgumentException($"Illegal class passed as substitution, found {value.GetType()} at col:{col}");
+        }
+    }
+
+    private static bool IsEmpty(SubstitutionType substType, object value)
+    {
+        if (value == null)
+        {
+            return true;
+        }
+        else
+        {
+            switch (substType)
+            {
+                case SubstitutionType.STRING:
+                    return string.IsNullOrEmpty((string)value);
+                case SubstitutionType.LIST:
+                    return ((List<string>)value).Count == 0;
+                case SubstitutionType.DICTIONARY:
+                    return ((Dictionary<string, string>)value).Count == 0;
+                default:
+                    return true;
+            }
+        }
+    }
+
+    // returns true if expansion happened
+    private static bool ExpandToken(
+            Modifier modifier,
+            string token,
+            bool composite,
+            int maxChar,
+            bool firstToken,
+            Dictionary<string, object> substitutions,
+            StringBuilder result,
+            int col)
+    {
+        if (string.IsNullOrEmpty(token))
+        {
+            throw new ArgumentException($"Found an empty token at col:{col}");
+        }
+
+        object value = substitutions[token];
+        if (value is int ||
+                value is long ||
+                value is float ||
+                value is double)
+        {
+            value = value.ToString();
+        }
+
+        SubstitutionType substType = GetSubstitutionType(value, col);
+        if (IsEmpty(substType, value))
+        {
+            return false;
+        }
+
+        if (firstToken)
+        {
+            AddPrefix(modifier, result);
+        }
+        else
+        {
+            AddSeparator(modifier, result);
+        }
+
+        switch (substType)
+        {
+            case SubstitutionType.STRING:
+                AddStringValue(modifier, token, (string)value, result, maxChar);
+                break;
+            case SubstitutionType.LIST:
+                AddListValue(modifier, token, (List<string>)value, result, maxChar, composite);
+                break;
+            case SubstitutionType.DICTIONARY:
+                AddDictionaryValue(modifier, token, (Dictionary<string, string>)value, result, maxChar, composite);
+                break;
+        }
+
+        return true;
+    }
+
+    private static bool AddStringValue(Modifier modifier, string token, string value, StringBuilder result, int maxChar)
+    {
+        AddValue(modifier, token, value, result, maxChar);
+        return true;
+    }
+
+    private static bool AddListValue(Modifier modifier, string token, List<string> value, StringBuilder result, int maxChar, bool composite)
+    {
+        bool first = true;
+        foreach (string v in value)
+        {
+            if (first)
+            {
+                AddValue(modifier, token, v, result, maxChar);
+                first = false;
+            }
+            else
+            {
+                if (composite)
+                {
+                    AddSeparator(modifier, result);
+                    AddValue(modifier, token, v, result, maxChar);
+                }
+                else
+                {
+                    result.Append(',');
+                    AddValueElement(modifier, token, v, result, maxChar);
+                }
+            }
+        }
+        return !first;
+    }
+
+    private static bool AddDictionaryValue(Modifier modifier, string token, Dictionary<string, string> value, StringBuilder result, int maxChar, bool composite)
+    {
+        bool first = true;
+        if (maxChar != -1)
+        {
+            throw new ArgumentException("Value trimming is not allowed on Dictionaries");
+        }
+        foreach (KeyValuePair<string, string> v in value)
+        {
+            if (composite)
+            {
+                if (!first)
+                {
+                    AddSeparator(modifier, result);
+                }
+                AddValueElement(modifier, token, v.Key, result, maxChar);
+                result.Append('=');
+            }
+            else
+            {
+                if (first)
+                {
+                    AddValue(modifier, token, v.Key, result, maxChar);
+                }
+                else
+                {
+                    result.Append(',');
+                    AddValueElement(modifier, token, v.Key, result, maxChar);
+                }
+                result.Append(',');
+            }
+            AddValueElement(modifier, token, v.Value, result, maxChar);
+            first = false;
+        }
+        return !first;
+    }
+}
---
+++ csharp/test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using stduritemplate;
+
+namespace UriTemplateConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: UriTemplateConverter <template_file> <data_file>");
+                return;
+            }
+
+            string templateFile = args[0];
+            string dataFile = args[1];
+
+            try
+            {
+                string jsonData = File.ReadAllText(dataFile);
+                var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonData);
+                
+                string template = File.ReadAllText(templateFile).Trim();
+                
+                try
+                {
+                    string result = StdUriTemplate.Expand(template, data);
+                    Console.Write(result);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine($"Error expanding template: {e.Message}");
+                    Console.Error.WriteLine(e.StackTrace);
+                    Console.WriteLine("false");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine($"File not found: {dataFile}");
+            }
+            catch (JsonException e)
+            {
+                Console.Error.WriteLine($"Error parsing JSON data: {e.Message}");
+            }
+        }
+    }
+}
