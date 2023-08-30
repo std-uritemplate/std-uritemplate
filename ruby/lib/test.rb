81389b351f56db3b0bcbb8e3f19edbad4369f909
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-08-30 17:59:06 +0200

    Initial Ruby implementation (#43)
    * Initial Ruby implementation
    
    * CI fixes
    
    * more
    
    * remove .ruby_version
    
    * try to use gems
    
    * minor
    
    * more canonical names
    
    * tweaks
    
    * more attempts
    
    * more
    
    * hammering
    
    * retry
    
    * minor
    
    * debug with tmate the action
    
    * fixing ruby CI
    
    * minor
    
    * finalize publishing of the gem

---
+++ ruby/lib/test.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'uri'
+require 'pathname'
+require_relative 'StdUriTemplate'
+
+template_file = ARGV[0]
+data_file = ARGV[1]
+
+begin
+  data = JSON.parse(File.read(data_file))
+rescue Errno::ENOENT
+  $stderr.puts("File '#{data_file}' not found.")
+  exit(1)
+rescue JSON::ParserError => e
+  $stderr.puts("Error parsing JSON data: #{e.message}")
+  exit(1)
+end
+
+begin
+  template = File.read(template_file).strip
+rescue Errno::ENOENT
+  $stderr.puts("File '#{template_file}' not found.")
+  exit(1)
+end
+
+begin
+  result = StdUriTemplate.expand(template, data)
+  puts result
+rescue StandardError => e
+  $stderr.puts("Error expanding template: #{e.message}")
+  $stderr.puts(e.backtrace.join("\n"))
+  puts 'false'
+end
---
+++ ruby/stduritemplate.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = "stduritemplate"
+  s.version     = ENV['VERSION'] || "0.0.0"
+  s.summary     = "stduritemplate"
+  s.description = "std-uritemplate implementation for Ruby"
+  s.authors     = ["Andrea Peruffo"]
+  s.email       = "andrea.peruffo1982@gmail.com"
+  s.files       = ["lib/StdUriTemplate.rb"]
+  s.homepage    =
+    "https://rubygems.org/gems/std-uritemplate"
+  s.license       = "APACHE-2"
+end
