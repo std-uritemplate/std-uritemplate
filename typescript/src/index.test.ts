ad20172b26e68a74ad65b790e0d2a2e194420eda
Musale Martin <martinmusale@microsoft.com>
2024-02-19 20:08:25 +0300

    add testing for the spec-examples-by-section.json file

--- typescript/src/index.test.ts
+++ typescript/src/index.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, expectTypeOf, test } from 'vitest'
 import { StdUriTemplate } from './index'
-import specExamples from '../../uritemplate-test/spec-examples.json'
+import specExamples from '../../uritemplate-test/spec-examples.json';
+import specExamplesBySection from '../../uritemplate-test/spec-examples-by-section.json';
 
 const levels = {
   "level": 1,
@@ -22,6 +23,19 @@ const specExamplesLevels = [
   "Level 4 Examples",
 ]
 
+const specExamplesBySectionLevels = [
+  "2.1 Literals",
+  "3.2.1 Variable Expansion",
+  "3.2.2 Simple String Expansion",
+  "3.2.3 Reserved Expansion",
+  "3.2.4 Fragment Expansion",
+  "3.2.5 Label Expansion with Dot-Prefix",
+  "3.2.6 Path Segment Expansion",
+  "3.2.7 Path-Style Parameter Expansion",
+  "3.2.8 Form-Style Query Expansion",
+  "3.2.9 Form-Style Query Continuation",
+]
+
 test('ensure browser mode is available', () => {
   expect(typeof window).not.toBe('undefined')
 })
@@ -36,11 +50,14 @@ describe("StdUriTemplate - expand", () => {
   })
 
   test("spec-examples.json exists", () => {
-
     expect(specExamples).toBeDefined();
     expect(Object.keys(specExamples)).toEqual(specExamplesLevels);
   })
 
+  test("spec-examples-by-json.json exists", () => {
+    expect(specExamplesBySection).toBeDefined();
+    expect(Object.keys(specExamplesBySection)).toEqual(specExamplesBySectionLevels);
+  })
 })
 
 describe.each(specExamplesLevels)('testing %s', (level: string) => {
@@ -63,6 +80,29 @@ describe.each(specExamplesLevels)('testing %s', (level: string) => {
       const result = StdUriTemplate.expand(template, variables);
       expectTypeOf(expected).toBeArray;
       expect(result).toStrictEqual(expected.pop());
+    })
+  });
+})
+
+describe.each(specExamplesBySectionLevels)('testing %s', (level: string) => {
+  const testcases = specExamplesBySection[level]["testcases"]
+  expect(testcases).toBeDefined();
+
+  const variables = specExamplesBySection[level]["variables"]
+  expect(variables).toBeDefined();
+
+  testcases.forEach((testcase: Array<Array<any>>) => {
+    const template = testcase[0] as unknown as string;
+    const expected = testcase[1] as unknown;
+    test(`StdUriTemplate.expand(${template}, ${JSON.stringify(variables)})`, () => {
+      const result = StdUriTemplate.expand(template, variables);
+      if(typeof expected === 'string'){
+        expectTypeOf(result).toBeString;
+        expect(result).toBe(expected)
+      }else if(Array.isArray(expected)){
+        expectTypeOf(expected).toBeArray;
+        expect(result).toStrictEqual(expected.pop());
+      }
     })
   });
 })
