dcc383a2b22d51dbf81052717f2707b8788eb026
Riccardo Forina <rforina@redhat.com>
2024-03-11 13:04:07 +0100

    Change variable name

--- typescript-mjs/init.sh
+++ typescript-mjs/init.sh
@@ -3,12 +3,12 @@
 # This directory is a placeholder needed to add a second test target for the typescript
 # project. To make the DX not suffer, let's run the init script of the typescript
 # project when running tests on this, so to have cold runs work ok without surprises.
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../typescript
+TYPESCRIPT_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../typescript
 
 # This is intended to be a customizable entrypoint for each language, it has to be generic enough
 (
-  cd ${SCRIPT_DIR} && \
-  rm -rf ${SCRIPT_DIR}/dist && \
+  cd ${TYPESCRIPT_SCRIPT_DIR} && \
+  rm -rf ${TYPESCRIPT_SCRIPT_DIR}/dist && \
   npm install && \
   npm run clean && \
   npm run build
--- typescript-mjs/test.sh
+++ typescript-mjs/test.sh
@@ -2,7 +2,7 @@
 
 # This directory is a placeholder needed to add a second test target for the typescript
 # project. Sources and tests are located in the typescript project.
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../typescript
+TYPESCRIPT_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../typescript
 
 # This is intended to be a customizable entrypoint for each language, it has to be generic enough
-node ${SCRIPT_DIR}/test.mjs.mjs $@
+node ${TYPESCRIPT_SCRIPT_DIR}/test.mjs.mjs $@
