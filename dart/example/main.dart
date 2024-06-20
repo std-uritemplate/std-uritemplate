69f276ee5ef334689642ae83fc7d5debfed07f36
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2024-06-21 00:59:10 +0300

    improve pub.dev score

---
+++ dart/example/main.dart
@@ -0,0 +1,17 @@
+import 'package:std_uritemplate/std_uritemplate.dart';
+
+void main() {
+  final substitutes = <String, Object?>{
+    'id': 'person',
+    'fields': ['id', 'name', 'picture'],
+    'long': 37.76,
+    'lat': -122.427,
+    'nullable': null,
+  };
+  
+  // prints: /person
+  print(StdUriTemplate.expand('{/id*}', substitutes));
+
+  // prints: /person?fields=id,name,picture
+  print(StdUriTemplate.expand('{/id*}{?fields,nullable}', substitutes));
+}
--- dart/test.dart
+++ dart/test.dart
@@ -7,36 +7,36 @@ import 'lib/std_uritemplate.dart';
 
 Future<void> main(List<String> args) async {
   if (args.length != 2) {
-    stderr.write("Usage: test.dart <template_file> <data_file>\n");
+    stderr.write('Usage: test.dart <template_file> <data_file>\n');
     exit(1);
   }
 
   final templateFile = args[0];
   final dataFile = args[1];
 
-  final dynamic data;
+  final Map<String, Object?> data;
+  
   try {
-    data = jsonDecode(File(dataFile).readAsStringSync());
+    data =
+        jsonDecode(File(dataFile).readAsStringSync()) as Map<String, Object?>;
   } catch (e, stack) {
-    stderr.write("Error loading data file: $e\n$stack\n");
+    stderr.write('Error loading data file: $e\n$stack\n');
     exit(1);
   }
 
-  if (data is Map && data.containsKey("nativedate")) {
-    data["nativedate"] =
-        DateTime.fromMillisecondsSinceEpoch(data["nativedate"]);
+  if (data['nativedate'] case final int nativeDate) {
+    data['nativedate'] = DateTime.fromMillisecondsSinceEpoch(nativeDate);
   }
 
-  if (data is Map && data.containsKey("nativedatetwo")) {
-    data["nativedatetwo"] =
-        DateTime.fromMillisecondsSinceEpoch(data["nativedatetwo"]);
+  if (data['nativedatetwo'] case final int nativeDateTwo) {
+    data['nativedatetwo'] = DateTime.fromMillisecondsSinceEpoch(nativeDateTwo);
   }
 
   final String template;
   try {
     template = File(templateFile).readAsStringSync().trim();
   } catch (e, stack) {
-    stderr.write("Error loading template file: $e\n$stack\n");
+    stderr.write('Error loading template file: $e\n$stack\n');
     exit(1);
   }
 
@@ -44,8 +44,8 @@ Future<void> main(List<String> args) async {
     final result = StdUriTemplate.expand(template, data);
     print(result);
   } catch (e, stack) {
-    stderr.write("Error: $e\n$stack\n");
+    stderr.write('Error: $e\n$stack\n');
 
-    print("false");
+    print('false');
   }
 }
