8e5bb11373207a1671046d85a90d6e22eb0bf5d3
Vincent Biret <vincentbiret@hotmail.com>
2024-08-16 14:51:09 -0400

    feat: implements get operator

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,6 +12,8 @@ fn validate_literal(c: char, col: u32) -> Result<(), String>
     }
 }
 
+#[derive(Debug)]
+#[derive(PartialEq)]
 enum Operator {
     NoOp,
     Plus,
@@ -21,6 +23,28 @@ enum Operator {
     Semicolon,
     QuestionMark,
     Amp
+}
+
+fn get_operator(c: char, mut token: String, col: u32) -> Result<Operator, String>
+{
+    match c {
+        '+' => Ok(Operator::Plus),
+        '#' => Ok(Operator::Hash),
+        '.' => Ok(Operator::Dot),
+        '/' => Ok(Operator::Slash),
+        ';' => Ok(Operator::Semicolon),
+        '?' => Ok(Operator::QuestionMark),
+        '&' => Ok(Operator::Amp),
+        _ => match validate_literal(c, col) {
+                Ok(()) => {
+                    token += &c.to_string();
+                    Ok(Operator::NoOp)
+                },
+                Err(e) => {
+                    Err(e)
+                }
+        },
+    }
 }
 
 pub fn add(left: u64, right: u64) -> u64 {
@@ -48,5 +72,24 @@ mod tests {
         assert!(validate_literal(':', 1).is_ok());
         assert!(validate_literal('~', 1).is_ok());
         assert!(validate_literal('-', 1).is_err());
+    }
+    #[test]
+    fn gets_operator() {
+        assert_eq!(get_operator('+', String::from(""), 1).unwrap(), Operator::Plus);
+        assert_eq!(get_operator('#', String::from(""), 1).unwrap(), Operator::Hash);
+        assert_eq!(get_operator('.', String::from(""), 1).unwrap(), Operator::Dot);
+        assert_eq!(get_operator('/', String::from(""), 1).unwrap(), Operator::Slash);
+        assert_eq!(get_operator(';', String::from(""), 1).unwrap(), Operator::Semicolon);
+        assert_eq!(get_operator('?', String::from(""), 1).unwrap(), Operator::QuestionMark);
+        assert_eq!(get_operator('&', String::from(""), 1).unwrap(), Operator::Amp);
+        assert_eq!(get_operator(' ', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('!', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('=', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('$', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('|', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('*', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator(':', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('~', String::from(""), 1).unwrap(), Operator::NoOp);
+        assert_eq!(get_operator('-', String::from(""), 1).unwrap_err(), "Invalid character '-' at column 1");
     }
 }
