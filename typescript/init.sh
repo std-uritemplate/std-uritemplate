3c5ca778c3122df74b372897578eff48e5649224
andreatp <andrea.peruffo1982@gmail.com>
2026-03-24 13:12:38 +0000

    unbuild requires TS 5.9

--- typescript/init.sh
+++ typescript/init.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 (
   cd ${SCRIPT_DIR} && \
   rm -rf ${SCRIPT_DIR}/dist && \
-  npm install && \
+  npm install --legacy-peer-deps && \
   npm run clean && \
   npm run build
 )
